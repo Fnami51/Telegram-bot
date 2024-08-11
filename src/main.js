a07cf7b1411df788dc5ac5c5ba59e5b1de6a33f5
Fnami51 <fnm5160@gmail.com>
2024-08-11 16:29:43 +0300

    v0.1.2 - Add message "Start"

--- src/main.js
+++ src/main.js
@@ -7,10 +7,18 @@ const {port, token, directusUrl, directusToken} = config
 
 const bot = new TelegramBot(token, { polling: true });
 
+let users = new Set();
+
 const app = express();
 app.get('/', (req, res) => {
     res.send('Бот работает');
   });
 app.listen(port, () => {
     console.log(`Сервер запущен на порту ${port}`);
+  });
+
+  bot.onText(/\/start/, (msg) => {
+    const chatId = msg.chat.id;
+    users.add(chatId);
+    bot.sendMessage(chatId, 'Бот добавлен');
   });
