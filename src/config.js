c0f121b4046a2160483c2757fb3d5e3a18c67f75
Fnami51 <fnm5160@gmail.com>
2024-08-11 14:21:16 +0300

    v0.1.0 - Add Express and TB

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 const config = {
+    port: 3000,
     token: '',
-    directusUrl: '',
+    directusUrl: 'https://',
     directusToken: ''
 }
 
--- src/main.js
+++ src/main.js
@@ -3,4 +3,14 @@ const axios = require('axios');
 const TelegramBot = require('node-telegram-bot-api');
 const config = require('./config')
 
-const {token, directusUrl, directusToken} = config
+const {port, token, directusUrl, directusToken} = config
+
+const bot = new TelegramBot(token, { polling: true });
+
+const app = express();
+app.get('/', (req, res) => {
+    res.send('Бот работает');
+  });
+app.listen(port, () => {
+    console.log(`Сервер запущен на порту ${port}`);
+  });
